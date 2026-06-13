15ca16e77cf293c7d161b36825b1614878882fe1
🔐Laurent M🔐 <laurent@data-shield.eu>
2026-06-13 22:28:53 +0200

    ⚡ 1.01.1 ⚡ : Minor fixes (Please, check release notes for more info)

--- src/universel/generate_dashboard.sh
+++ src/universel/generate_dashboard.sh
@@ -45,7 +45,7 @@ generate_dashboard() {
 set -euo pipefail
 
 # --- VERSION CONFIGURATION ---
-SYSWARDEN_VERSION="v1.01.0"
+SYSWARDEN_VERSION="v1.01.1"
 
 DATA_FILE="/etc/syswarden/ui/data.json"
 
--- tools/syswarden-waf.sh
+++ tools/syswarden-waf.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # SysWarden WAF - Enterprise ModSecurity v3 & OWASP CRS Deployment Module
-# Designed for SysWarden v1.01.0 Architecture
+# Designed for SysWarden v1.01.1 Architecture
 # Compatibility: FreeBSD 14.4+
 
 # --- SAFETY FIRST ---
